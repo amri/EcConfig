49d849d2e0c237924249e25b577fd7f8aab534ce
Laurent Babin <spywen@hotmail.fr>
2015-11-01 15:47:58 +0100

    Logo + nuspec configs for release 1.0.0

--- EcConfig.Example.Console/Program.cs
+++ EcConfig.Example.Console/Program.cs
@@ -8,17 +8,17 @@ namespace EcConfig.Example.Console
         private static void Main(string[] args)
         {
             //Get property from app.config
-            System.Console.WriteLine("=== Get property from app.config file: ===");
-            System.Console.WriteLine("appSettingsProperty value: {0}", ConfigurationManager.AppSettings["appSettingsProperty"]);
+            System.Console.WriteLine(@"=== Get property from app.config file: ===");
+            System.Console.WriteLine(@"appSettingsProperty value: {0}", ConfigurationManager.AppSettings["appSettingsProperty"]);
 
             System.Console.WriteLine();
             //Get properties from default.config thanks to EcConfig (default.config file is the default filename configuration for EcConfig)
-            System.Console.WriteLine("=== Get property from default.config file: ===");
-            System.Console.WriteLine("rootProperty value: {0}", Config.Get("rootProperty"));
-            System.Console.WriteLine("page1/title value: {0}", Config.Get("page1.title"));
-            System.Console.WriteLine("page1/part1/title value: {0}", Config.Get("page1.part1.title"));
-            System.Console.WriteLine("page1/part2/title value: {0}", Config.Get("page1.part2.title"));
-            System.Console.WriteLine("number1 + number2 value: {0}", Config.Get("number1").ToInt() + Config.Get("number2").ToInt());
+            System.Console.WriteLine(@"=== Get property from default.config file: ===");
+            System.Console.WriteLine(@"rootProperty value: {0}", Config.Get("rootProperty"));
+            System.Console.WriteLine(@"page1/title value: {0}", Config.Get("page1.title"));
+            System.Console.WriteLine(@"page1/part1/title value: {0}", Config.Get("page1.part1.title"));
+            System.Console.WriteLine(@"page1/part2/title value: {0}", Config.Get("page1.part2.title"));
+            System.Console.WriteLine(@"number1 + number2 value: {0}", Config.Get("number1").ToInt() + Config.Get("number2").ToInt());
 
             System.Console.ReadLine();
         }
